952bd691a8afa73d395d75a4c2bdc7e06b9ed5e1
Albert Chen <49454026+albertchen-okta@users.noreply.github.com>
2021-07-23 09:45:10 -0700

    one more update to the Title text

--- packages/@okta/vuepress-theme-prose/global-components/DeveloperDayBanner.vue
+++ packages/@okta/vuepress-theme-prose/global-components/DeveloperDayBanner.vue
@@ -16,7 +16,7 @@
                 developer-day-banner--header-no-margin-large
               "
             >
-              Okta + Auth0 DeveloperDay 2021
+              Developer Day
             </h2>
             <h2
               class="
@@ -24,7 +24,7 @@
                 developer-day-banner--header-no-margin-small
               "
             >
-              DeveloperDay 2021
+              Developer Day
             </h2>
             <div class="developer-day-banner--cta">
               <span class="developer-day-banner--cta-large">
